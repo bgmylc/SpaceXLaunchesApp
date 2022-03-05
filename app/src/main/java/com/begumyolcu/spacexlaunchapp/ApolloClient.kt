085b5505fc36db3060286dd32e8e68f7f120a7b5
Begum Yolcu <begum.yolcu@tedu.edu.tr>
2022-03-05 20:23:49 +0300

    Apollo Client

---
+++ app/src/main/java/com/begumyolcu/spacexlaunchapp/ApolloClient.kt
@@ -0,0 +1,7 @@
+package com.begumyolcu.spacexlaunchapp
+
+import com.apollographql.apollo3.ApolloClient
+
+val apolloClient = ApolloClient.Builder()
+    .serverUrl("https://api.spacex.land/graphql/")
+    .build()
--- app/src/main/java/com/begumyolcu/spacexlaunchapp/ui/HomeListFragment.kt
+++ app/src/main/java/com/begumyolcu/spacexlaunchapp/ui/HomeListFragment.kt
@@ -1,23 +1,41 @@
 package com.begumyolcu.spacexlaunchapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import com.begumyolcu.spacexlaunchapp.LaunchesPastListQuery
 import com.begumyolcu.spacexlaunchapp.R
+import com.begumyolcu.spacexlaunchapp.apolloClient
 import com.begumyolcu.spacexlaunchapp.databinding.FragmentHomeListBinding
 
 
 class HomeListFragment : Fragment() {
     private lateinit var binding: FragmentHomeListBinding
+    private val viewModel by lazy { LaunchesPastViewModel() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_list, container, false)
+
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenResumed {
+            val response = apolloClient.query(LaunchesPastListQuery()).execute()
+
+            Log.d("LaunchList", "Success ${response.data}")
+        }
     }
 
 }
